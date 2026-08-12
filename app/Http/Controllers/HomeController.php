59de0d0570e495b42d0c4d40a4a9b265ba6dd801
johan <jemesav@eafit.edu.co>
2026-08-12 11:11:25 -0500

    Orden al caos Fase 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,17 +10,17 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+
     public function about(): View
     {
-        $data1 = "About us - Online Store";
-        $data2 = "About us";
-        $description = "This is an about page ...";
-        $author = "Developed by: Your Name";
+        $data1 = 'About us - Online Store';
+        $data2 = 'About us';
+        $description = 'This is an about page ...';
+        $author = 'Developed by: Your Name';
 
-        return view('home.about')->with("title", $data1)
-            ->with("subtitle", $data2)
-            ->with("description", $description)
-            ->with("author", $author);
+        return view('home.about')->with('title', $data1)
+            ->with('subtitle', $data2)
+            ->with('description', $description)
+            ->with('author', $author);
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 $PathController = 'App\Http\Controllers\\';
 
-Route::get('/', $PathController . 'HomeController@index')->name("home.index");
-Route::get('/about', $PathController . 'HomeController@about')->name("home.about");
-Route::get('/products', $PathController . 'ProductController@index')->name("product.index");
-Route::get('/products/create', $PathController . 'ProductController@create')->name("product.create");
-Route::post('/products/save', $PathController . 'ProductController@save')->name("product.save");
-Route::get('/products/{id}', $PathController . 'ProductController@show')->name("product.show");
+Route::get('/', $PathController.'HomeController@index')->name('home.index');
+Route::get('/about', $PathController.'HomeController@about')->name('home.about');
+Route::get('/products', $PathController.'ProductController@index')->name('product.index');
+Route::get('/products/create', $PathController.'ProductController@create')->name('product.create');
+Route::post('/products/save', $PathController.'ProductController@save')->name('product.save');
+Route::get('/products/{id}', $PathController.'ProductController@show')->name('product.show');
