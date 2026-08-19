4b0377685446e6b23b7e36cc572c95753e9b8093
johan <jemesav@eafit.edu.co>
2026-08-18 19:31:38 -0500

    2do Tutorial

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,24 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ['id' => '1', 'name' => 'TV', 'description' => 'Best TV'],
-        ['id' => '2', 'name' => 'iPhone', 'description' => 'Best iPhone'],
-        ['id' => '3', 'name' => 'Chromecast', 'description' => 'Best Chromecast'],
-        ['id' => '4', 'name' => 'Glasses', 'description' => 'Best Glasses'],
-    ];
-
     public function index(): View
     {
         $viewData = [];
         $viewData['title'] = 'Products - Online Store';
         $viewData['subtitle'] = 'List of products';
-        $viewData['products'] = ProductController::$products;
+        $viewData['products'] = Product::all();
 
         return view('product.index')->with('viewData', $viewData);
     }
@@ -27,7 +22,7 @@ class ProductController extends Controller
     public function show(string $id): View
     {
         $viewData = [];
-        $product = ProductController::$products[$id - 1];
+        $product = Product::findOrFail($id);
         $viewData['title'] = $product['name'].' - Online Store';
         $viewData['subtitle'] = $product['name'].' - Product information';
         $viewData['product'] = $product;
@@ -43,13 +38,15 @@ class ProductController extends Controller
         return view('product.create')->with('viewData', $viewData);
     }
 
-    public function save(Request $request)
+    public function save(Request $request): RedirectResponse
     {
         $request->validate([
             'name' => 'required',
             'price' => 'required|numeric|gt:0',
         ]);
 
-        return back()->with('success', 'completed successfully!');
+        Product::create($request->only(['name', 'price']));
+
+        return back()->with('success', 'Product created successfully!');
     }
 }
